ec01652273b978886537b14a8a9c263b6182f700
Abdelnacer Jabri <73711840+faizandev12@users.noreply.github.com>
2025-03-19 23:48:28 -0400

    commit

--- index.ts
+++ index.ts
@@ -1,9 +1,54 @@
-import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
+import * as pulumi from "@pulumi/pulumi";
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.BucketV2("my-bucket");
+// Create an S3 bucket
+const bucket = new aws.s3.Bucket("static-web-jabri-bucket", {
+    website: {
+        indexDocument: "index.html",
+    },
+});
 
-// Export the name of the bucket
-export const bucketName = bucket.id;
+// Upload a sample index.html
+const bucketObject = new aws.s3.BucketObject("index.html", {
+    bucket: bucket.id,
+    source: new pulumi.asset.FileAsset("index.html"), // Create this file locally
+    contentType: "text/html",
+});
+
+// Create CloudFront distribution
+const distribution = new aws.cloudfront.Distribution("static-website-distribution", {
+    enabled: true,
+    origins: [{
+        originId: bucket.arn,
+        domainName: bucket.websiteEndpoint,
+        customOriginConfig: {
+            originProtocolPolicy: "http-only",
+            httpPort: 80,
+            httpsPort: 443,
+            originSslProtocols: ["TLSv1.2"],
+        },
+    }],
+    defaultRootObject: "index.html",
+    defaultCacheBehavior: {
+        targetOriginId: bucket.arn,
+        viewerProtocolPolicy: "redirect-to-https",
+        allowedMethods: ["GET", "HEAD"],
+        cachedMethods: ["GET", "HEAD"],
+        forwardedValues: {
+            queryString: false,
+            cookies: { forward: "none" },
+        },
+    },
+    restrictions: {
+        geoRestriction: {
+            restrictionType: "none",
+        },
+    },
+    viewerCertificate: {
+        cloudfrontDefaultCertificate: true,
+    },
+});
+
+// Export URLs
+export const bucketUrl = bucket.websiteEndpoint;
+export const distributionUrl = distribution.domainName;
